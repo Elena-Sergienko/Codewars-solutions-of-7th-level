7fb6c1779793b1c89fdc2ee12414e8e88afec4ac
Elena Sergienko <68942106+Elena-Sergienko@users.noreply.github.com>
2020-08-11 23:13:41 -0700

    Add Convert an array
    Add Convert an array of strings to array of numbers

---
+++ Convert an array.js
@@ -0,0 +1,16 @@
+// 7 kyu
+// Convert an array of strings to array of numbers
+// https://www.codewars.com/kata/5783d8f3202c0e486c001d23
+// Some really funny web dev gave you an array of numbers from his API response as an array of strings!
+// You need to cast the whole array to the correct type.
+// Create the function
+// JavaScript: toNumberArray(array)
+// Ruby: toNumberArray(array)
+// C#: ToDoubleArray(array)
+// that takes as a parameter an array of numbers represented as strings and outputs an array of numbers.
+// ie:["1", "2", "3"] to [1, 2, 3]
+// Note that you can receive floats as well.
+
+function toNumberArray(stringarray) {
+    return stringarray.map(el => +el)
+}
